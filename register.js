0774e9bceb001df074a0854fa6a0f4225f9363ea
edo1978italia <cianca.edo@gmail.com>
2025-06-24 22:37:58 +0900

    Add files via upload

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from './config.js';
+import firebaseConfig from "./config.js";
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 import {
   getAuth,
